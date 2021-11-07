a7aa1db30c15d83cb34fe08caffdec150a6da223
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2021-11-07 20:44:40 +0800

    upload initial files

---
+++ ass2/helpers.sql
@@ -0,0 +1,5 @@
+-- COMP3311 21T3 Ass2 ... extra database definitions
+-- add any views or functions you need into this file
+-- note: it must load without error into a freshly created mymyunsw database
+-- you must submit this even if you add nothing to it
+
---
+++ ass2/query.sql
@@ -0,0 +1,27 @@
+-- Q1
+-- select
+--     subjects.code as subject,
+--     terms.code as term,
+--     subjects.name as title,
+--     course_enrolments.mark,
+--     course_enrolments.grade,
+--     subjects.uoc
+-- from course_enrolments
+-- join courses on (course_enrolments.course = courses.id)
+-- join subjects on (courses.subject = subjects.id)
+-- join terms on (courses.term = terms.id)
+-- where course_enrolments.student = '5143550'
+-- order by terms.code, subjects.code;
+
+-- Q2
+select
+    programs.code as program,
+    orgunits.longname as offeredby,
+    rules.name as rule_name,
+    aog.definition as course_requirements
+from program_rules pr
+join programs on (pr.program = programs.id)
+join orgunits on (programs.offeredby = orgunits.id)
+join rules on (pr.rule = rules.id)
+join academic_object_groups aog on (rules.ao_group = aog.id)
+where programs.code = '3778';
